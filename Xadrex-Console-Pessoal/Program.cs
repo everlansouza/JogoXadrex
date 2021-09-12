3fb51519c6725901fea08efb8a7cdaba03456ade
Everlan Souza <everlansoouza@gmail.com>
2021-09-11 21:24:29 -0300

    Iniciando o projeto, criando um jogo de xadrez nas linhas de comando em C#

---
+++ Xadrex-Console-Pessoal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xadrex_Console_Pessoal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
