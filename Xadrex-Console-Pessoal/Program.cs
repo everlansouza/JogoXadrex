e7f2acc3c0dfca1255f2a167a6f9b639145e5f8e
Everlan Souza <everlansoouza@gmail.com>
2021-09-12 21:17:53 -0300

    Implementado o esquemas de cores das peças para diferenciar o adiversario

--- Xadrex-Console-Pessoal/Program.cs
+++ Xadrex-Console-Pessoal/Program.cs
@@ -16,6 +16,7 @@ namespace Xadrex_Console_Pessoal
                 tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
                 tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 2));
 
+                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Branca), new Posicao(3, 5));
 
                 Tela.imprimirTabuleiro(tabuleiro);
             }
